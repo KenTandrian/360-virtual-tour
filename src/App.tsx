d0bc5e1a1a5f4333897c95d1b744e5431da43c1a
KenTandrian <kenricktan11@gmail.com>
2023-07-08 21:47:03 +0700

    feat(routing): use createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Index from './pages';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
 import Page3 from './pages/page3';
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index/>,
+    children: [
+      { index: true, element: <Page1 /> },
+      { path: 'page2', element: <Page2 /> },
+      { path: 'page3', element: <Page3 /> }
+    ]
+  },
+], {
+  basename: process.env.PUBLIC_URL,
+});
+
 const App = () => {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path='/' element={<Index/>}>
-          <Route index element={<Page1 />} />
-          <Route path='page2' element={<Page2 />} />
-          <Route path='page3' element={<Page3 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
