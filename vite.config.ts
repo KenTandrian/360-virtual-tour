e7d88bd9da439b3d0898212b5f4916b60a11bdf0
KenTandrian <kenricktan11@gmail.com>
2023-10-05 23:40:46 +0700

    feat(pwa): setup sw registration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/360-virtual-tour/",
-  plugins: [react()],
+  build: {
+    sourcemap: process.env.SOURCE_MAP === "true",
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      filename: "serviceWorker.ts",
+      srcDir: "src",
+      strategies: "injectManifest",
+      manifestFilename: "manifest.json",
+    }),
+  ],
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
