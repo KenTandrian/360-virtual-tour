61e881e78f571b2b1ae9e03d7bce0139527ec65d
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-01-13 03:49:56 +0000

    fix(perf): use lazy loading for error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Index from "./pages";
-import ErrorPage from "./error";
 import "./App.css";
 
+const ErrorPage = React.lazy(() => import("./error"));
 const Page1 = React.lazy(() => import("./pages/page1"));
 const Page2 = React.lazy(() => import("./pages/page2"));
 const Page3 = React.lazy(() => import("./pages/page3"));
