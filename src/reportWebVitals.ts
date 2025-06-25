27a0c4c0ce0afb82fc3719db0a95264046a9ba13
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2025-06-25 16:09:50 +0000

    fix(app): changes on web-vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,13 @@
-import { ReportCallback } from "web-vitals";
+import { MetricType } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+export default function reportWebVitals(onPerfEntry?: (metric: MetricType) => void) {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+    import("web-vitals").then(({ onCLS, onFCP, onINP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry);
-      onFID(onPerfEntry);
       onFCP(onPerfEntry);
+      onINP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
     });
   }
-};
-
-export default reportWebVitals;
+}
